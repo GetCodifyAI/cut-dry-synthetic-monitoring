f045aa5fa63a34082adeb13494d2eea27144b0bf
senuri-mr <senuri@cutanddry.com>
2024-10-16 14:22:48 +0530

    Modified

--- src/main/java/com/cutanddry/qa/pages/CustomersPage.java
+++ src/main/java/com/cutanddry/qa/pages/CustomersPage.java
@@ -6,7 +6,7 @@ public class CustomersPage extends LoginPage {
     By tbx_searchCustomers = By.xpath("//input[@placeholder='Search Customers']");
     String btnOrderGuide = "//td[text()='CODE']/../td[8]//button";
     By lbl_itemNameList = By.xpath("//td//span/div[@data-tip='View Product Details']");
-    By lbl_itemDetails = By.xpath("//tbody/tr[2]");
+    By lbl_itemDetails = By.xpath("//tbody[@class='border-0']");
     By btn_increaseQtyFirstRow = By.xpath("//tr[1]/td[8]/div/div/div/div[3]");
     By btn_decreaseQtyFirstRow = By.xpath("//tr[1]/td[8]/div/div/div/div[1]");
     By btn_increaseQtySecondRow = By.xpath("//tr[2]/td[8]/div/div/div/div[3]");
